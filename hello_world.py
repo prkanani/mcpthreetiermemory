3d1480c785140763dea2cc70e23c8ee06d1d3ccf
prkanani <prkanani@microsoft.com>
2026-02-17 21:35:39 -0800

    update hello_world.py to add 2 numbers

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,8 @@
-def main():
-    print("Hello, World!")
+def add_two_numbers(a, b):
+    return a + b
 
 if __name__ == "__main__":
-    main()
+    num1 = 5
+    num2 = 7
+    result = add_two_numbers(num1, num2)
+    print(f"The sum of {num1} and {num2} is {result}")
